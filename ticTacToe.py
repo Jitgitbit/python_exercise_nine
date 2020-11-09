eeb6dee8a80c55e195f439736970cf4b262c2bc2
Jitgitbit <rowowolf@gmail.com>
2020-11-09 16:17:13 +0100

    player move done

--- ticTacToe.py
+++ ticTacToe.py
@@ -34,3 +34,21 @@ def isBoardFull(board):
 
 def isWinner(board, letter):
     return (board[1] == letter and board[2] == letter and board[3] == letter) or (board[4] == letter and board[5] == letter and board[6] == letter) or (board[7] == letter and board[8] == letter and board[9] == letter) or (board[1] == letter and board[4] == letter and board[7] == letter) or (board[2] == letter and board[5] == letter and board[8] == letter) or (board[3] == letter and board[6] == letter and board[9] == letter) or (board[1] == letter and board[5] == letter and board[9] == letter) or (board[3] == letter and board[5] == letter and board[7] == letter)
+
+def playerMove():
+	run = True
+	while run:
+		move = input("Please select a position to enter the X between 1 and 9: ")
+		try:
+			move = int(move)
+			if move > 0 and move < 10:
+				if spaceIsFree(move):
+					run = False
+					insertLetter('X', move)
+				else:
+					print("Sorry, this position is already taken.")
+			else:
+				print("Please type a number between 1 and 9.")
+
+		except:
+			print("Please type a number!")
